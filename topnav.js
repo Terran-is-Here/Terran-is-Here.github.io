682516ccdb952361b78a0759da8cc4a804204065
Terranium <57087245+Terran-is-Here@users.noreply.github.com>
2023-06-15 12:55:31 -0400

    fix #4

--- topnav.js
+++ topnav.js
@@ -1,18 +1,20 @@
 
-window.addEventListener("DOMContentLoaded", (event) => {
+window.addEventListener("DOMContentLoaded", (event) => { //code to be loaded when DOM is fully loaded
     console.log("DOM fully loaded and parsed");
     var navbar = document.getElementById("navbar");
     var sticky = navbar.offsetTop;
+    
+    function myFunction() {
+      if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+      } else {
+        navbar.classList.remove("sticky");
+      }
+    }
+
     window.onscroll = function() {myFunction()};
+
+
   });
 
   window.onscroll = function() {myFunction()};
-
-var forcedep = navbar
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
