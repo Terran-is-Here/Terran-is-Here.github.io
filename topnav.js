3b13d312b6bbbc0663aa37a447139c4a47a93282
Terranium <57087245+Terran-is-Here@users.noreply.github.com>
2023-06-15 12:38:11 -0400

    inserted lorem ipsum, attempted fix to topnav.js

--- topnav.js
+++ topnav.js
@@ -1,5 +1,10 @@
-window.onscroll = function() {myFunction()};
 
+window.addEventListener("DOMContentLoaded", (event) => {
+    console.log("DOM fully loaded and parsed");
+    window.onscroll = function() {myFunction()};
+  });
+
+  window.onscroll = function() {myFunction()};
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
